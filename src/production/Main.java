3dcacc726497efe9213893f09b5986334a879470
njmartina4082 <njmartina1@gmail.com>
2018-11-16 11:20:47 -0500

    JPP step 20, still need up 18 and 19

--- src/production/Main.java
+++ src/production/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
     MoviePlayerDriver.testMoviePlayer();
     PlayerDriver.testPlayer();
+
+    ProcessFiles f = new ProcessFiles();
+
+    try {
+      f.WriteFile("test222");
+    } catch (Exception ex){
+      System.out.println("could not write");
+    }
 }
 
 }
